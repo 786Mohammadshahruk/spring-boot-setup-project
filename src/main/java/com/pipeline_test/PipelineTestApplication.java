cdffea61fbecd0256fcf861f5f6c7b51d2fed667
Mohammad Shahruk <mshahruk57@gmail.com>
2024-10-08 18:31:31 +0530

    Hello

--- src/main/java/com/pipeline_test/PipelineTestApplication.java
+++ src/main/java/com/pipeline_test/PipelineTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PipelineTestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		SpringApplication.run(PipelineTestApplication.class, args);
 	}
 
